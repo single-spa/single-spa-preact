db8e1ef32d8de0ff2e6fae7d4fa401b760a32e1c
Kent McLean <kent.mclean@canopytax.com>
2016-12-07 16:01:20 -0700

    initial commit

---
+++ src/single-spa-preact.js
@@ -0,0 +1,71 @@
+const defaultOpts = {
+	// required opts
+	preact: null,
+	rootComponent: null,
+	domElementGetter: null,
+}
+
+export default function singleSpaPreact(userOpts) {
+	if (typeof userOpts !== 'object') {
+		throw new Error(`single-spa-preact requires a configuration object`);
+	}
+
+	const opts = {
+		...defaultOpts,
+		...userOpts,
+	};
+
+	if (!opts.preact) {
+		throw new Error(`single-spa-preact must be passed opts.preact`);
+	}
+
+	if (!opts.rootComponent) {
+		throw new Error(`single-spa-preact must be passed opts.rootComponent`);
+	}
+
+	if (!opts.domElementGetter) {
+		throw new Error(`single-spa-preact must be passed opts.domElementGetter function`);
+	}
+
+	return {
+		bootstrap: bootstrap.bind(null, opts),
+		mount: mount.bind(null, opts),
+		unmount: unmount.bind(null, opts),
+	};
+}
+
+function bootstrap(opts) {
+	return Promise.resolve();
+}
+
+function mount(opts) {
+	return new Promise((resolve, reject) => {
+		opts.preact.render(
+			opts.preact.h(opts.rootComponent),
+			getRootDomEl(opts)
+		);
+
+		resolve();
+	});
+}
+
+function unmount(opts) {
+	return new Promise((resolve, reject) => {
+		opts.preact.render(
+			null, // see https://github.com/developit/preact/issues/53
+			getRootDomEl(opts),
+		);
+
+		resolve();
+	});
+}
+
+function getRootDomEl(opts) {
+	const el = opts.domElementGetter();
+
+	if (!el) {
+		throw new Error(`single-spa-preact: domElementGetter function did not return a valid dom element`);
+	}
+
+	return el;
+}
