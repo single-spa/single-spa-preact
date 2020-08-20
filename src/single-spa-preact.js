182ac5182a2249a7420c845a407fe860d492f6d7
Michael Ellis <39313898+mellis481@users.noreply.github.com>
2020-08-20 13:03:18 -0400

    Make domElementGetter optional (#5)
    * Make domElementGetter optional
    
    * Corrected formatting

--- src/single-spa-preact.js
+++ src/single-spa-preact.js
@@ -1,74 +1,119 @@
 const defaultOpts = {
-	// required opts
-	preact: null,
-	rootComponent: null,
-	domElementGetter: null,
-}
+  // required opts
+  preact: null,
+  rootComponent: null,
+
+  // optional opts
+  domElementGetter: null,
+};
 
 export default function singleSpaPreact(userOpts) {
-	if (typeof userOpts !== 'object') {
-		throw new Error(`single-spa-preact requires a configuration object`);
-	}
+  if (typeof userOpts !== "object") {
+    throw new Error(`single-spa-preact requires a configuration object`);
+  }
 
-	const opts = {
-		...defaultOpts,
-		...userOpts,
-	};
+  const opts = {
+    ...defaultOpts,
+    ...userOpts,
+  };
 
-	if (!opts.preact) {
-		throw new Error(`single-spa-preact must be passed opts.preact`);
-	}
+  if (!opts.preact) {
+    throw new Error(`single-spa-preact must be passed opts.preact`);
+  }
 
-	if (!opts.rootComponent) {
-		throw new Error(`single-spa-preact must be passed opts.rootComponent`);
-	}
+  if (!opts.rootComponent) {
+    throw new Error(`single-spa-preact must be passed opts.rootComponent`);
+  }
 
-	if (!opts.domElementGetter) {
-		throw new Error(`single-spa-preact must be passed opts.domElementGetter function`);
-	}
-
-	return {
-		bootstrap: bootstrap.bind(null, opts),
-		mount: mount.bind(null, opts),
-		unmount: unmount.bind(null, opts),
-	};
+  return {
+    bootstrap: bootstrap.bind(null, opts),
+    mount: mount.bind(null, opts),
+    unmount: unmount.bind(null, opts),
+  };
 }
 
 function bootstrap(opts) {
-	return Promise.resolve();
+  return Promise.resolve();
 }
 
 function mount(opts, props) {
-	return new Promise((resolve, reject) => {
-		opts.renderedNode = opts.preact.render(
-			opts.preact.h(opts.rootComponent, props, null),
-			getRootDomEl(opts),
-		);
+  return new Promise((resolve, reject) => {
+    const domElementGetter = chooseDomElementGetter(opts, props);
 
-		resolve();
-	});
+    if (typeof domElementGetter !== "function") {
+      throw new Error(
+        `single-spa-preact: the domElementGetter for preact application '${
+          props.appName || props.name
+        }' is not a function`
+      );
+    }
+
+    opts.renderedNode = opts.preact.render(
+      opts.preact.h(opts.rootComponent, props, null),
+      getRootDomEl(domElementGetter, props)
+    );
+
+    resolve();
+  });
 }
 
-function unmount(opts) {
-	return new Promise((resolve, reject) => {
-		opts.preact.render(
-			'', // see https://github.com/developit/preact/issues/53
-			getRootDomEl(opts),
-			opts.renderedNode,
-		);
+function unmount(opts, props) {
+  return new Promise((resolve, reject) => {
+    const domElementGetter = chooseDomElementGetter(opts, props);
 
-		delete opts.renderedNode;
+    opts.preact.render(
+      "", // see https://github.com/developit/preact/issues/53
+      getRootDomEl(domElementGetter, opts),
+      opts.renderedNode
+    );
 
-		resolve();
-	});
+    delete opts.renderedNode;
+
+    resolve();
+  });
 }
 
-function getRootDomEl(opts) {
-	const el = opts.domElementGetter();
+function getRootDomEl(domElementGetter, props) {
+  const el = domElementGetter(props);
 
-	if (!el) {
-		throw new Error(`single-spa-preact: domElementGetter function did not return a valid dom element`);
-	}
+  if (!el) {
+    throw new Error(
+      `single-spa-preact: domElementGetter function did not return a valid dom element`
+    );
+  }
 
-	return el;
+  return el;
+}
+
+function chooseDomElementGetter(opts, props) {
+  if (props.domElement) {
+    return () => props.domElement;
+  } else if (props.domElementGetter) {
+    return props.domElementGetter;
+  } else if (opts.domElementGetter) {
+    return opts.domElementGetter;
+  } else {
+    return defaultDomElementGetter(props);
+  }
+}
+
+function defaultDomElementGetter(props) {
+  const appName = props.appName || props.name;
+  if (!appName) {
+    throw Error(
+      `single-spa-preact was not given an application name as a prop, so it can't make a unique dom element container for the preact application`
+    );
+  }
+  const htmlId = `single-spa-application:${appName}`;
+
+  return function defaultDomEl() {
+    let domElement = document.getElementById(htmlId);
+    if (!domElement) {
+      domElement = document.createElement("div");
+      domElement.id = htmlId;
+      document.body.appendChild(domElement);
+    }
+
+    return domElement;
+  };
 }
